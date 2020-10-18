df98e763779fbb4161290b2c4a718b0468f77f4b
DenisSvinkovskij <1911.denis55@gmail.com>
2020-10-18 10:54:43 +0300

    add more event-listeners

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const refs = {
   modal: document.querySelector('.js-lightbox'),
   closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
   modalImg: document.querySelector('.lightbox__image'),
+  overlay: document.querySelector('div.lightbox__overlay'),
 };
 
 refs.list.innerHTML = galleryItems
@@ -25,7 +26,15 @@ refs.list.innerHTML = galleryItems
   )
   .join('');
 
-refs.list.addEventListener('click', e => {
+refs.list.addEventListener('click', openModalImg);
+
+refs.closeModalBtn.addEventListener('click', closeModal);
+
+refs.overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', keyboardPress);
+
+function openModalImg(e) {
   e.preventDefault();
   if (e.currentTarget !== e.target) {
     refs.modal.classList.add('is-open');
@@ -36,10 +45,50 @@ refs.list.addEventListener('click', e => {
       }
     });
   }
-});
+}
 
-refs.closeModalBtn.addEventListener('click', () => {
+function closeModal() {
   refs.modal.classList.remove('is-open');
   refs.modalImg.src = '';
   refs.modalImg.alt = '';
-});
+}
+
+function keyboardPress(event) {
+  const urlsArr = galleryItems.map(el => el.original);
+  const altsArr = galleryItems.map(el => el.description);
+
+  if (event.code === 'Escape') {
+    closeModal();
+  }
+
+  if (event.code === 'ArrowRight') {
+    for (let i = 0; i < urlsArr.length; i += 1) {
+      if (refs.modalImg.src === urlsArr[urlsArr.length - 1]) {
+        refs.modalImg.src = `${urlsArr[0]}`;
+        refs.modalImg.alt = `${altsArr[0]}`;
+        return;
+      }
+
+      if (refs.modalImg.src === urlsArr[i]) {
+        refs.modalImg.src = `${urlsArr[i + 1]}`;
+        refs.modalImg.alt = `${altsArr[i + 1]}`;
+        return;
+      }
+    }
+  }
+  if (event.code === 'ArrowLeft') {
+    for (let i = 0; i < urlsArr.length; i += 1) {
+      if (refs.modalImg.src === urlsArr[0]) {
+        refs.modalImg.src = `${urlsArr[urlsArr.length - 1]}`;
+        refs.modalImg.alt = `${altsArr[altsArr.length - 1]}`;
+        return;
+      }
+
+      if (refs.modalImg.src === urlsArr[i]) {
+        refs.modalImg.src = `${urlsArr[i - 1]}`;
+        refs.modalImg.alt = `${altsArr[i - 1]}`;
+        return;
+      }
+    }
+  }
+}
